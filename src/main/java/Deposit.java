69e8f5e81d9cedf758591526898dc6a54fa8c1a5
Zanetta Norris <zanettanorris@gmail.com>
2020-03-15 23:43:02 -0400

    added project structure

---
+++ src/main/java/Deposit.java
@@ -0,0 +1,12 @@
+package java;
+
+public interface Deposit {
+    
+public Double Amount = null;
+    
+public double deposit(double deposit);
+
+public void roundUp(double r);
+
+public void newBalance(double b);
+}
